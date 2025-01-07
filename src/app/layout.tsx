4b9ccb093f02ae01defadfa59efe92f7068a9516
Samuel Wasswa <latimerscope@gmail.com>
2025-01-08 00:07:13 +0300

    Remove unused imports

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,16 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Inter } from "next/font/google";
+import {  Inter } from "next/font/google";
 import "./globals.css";
 import Link from "next/link";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+
 
 const inter = Inter({
   variable: "--font-inter",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+
 
 export const metadata: Metadata = {
   title: "Welcome to GPT Chat",
